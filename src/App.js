df4d763323aa5cd98674f19039947ea40dd9636d
Raul Veras <verasraul@gmail.com>
2024-09-04 13:17:50 -0400

    fixing rebase/merge issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import GetData from "./compontes/GetData";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import "./App.css";
 
 const App = () => {
@@ -14,6 +15,14 @@ const App = () => {
     <div>
       <h1>Gorrila</h1>
 >>>>>>> 42c070d (done with javascript part)
+=======
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app-con">
+      <h3>Korilla</h3>
+>>>>>>> c14778f (newUpdat onhpmework)
       <GetData />
     </div>
   );
--- src/compontes/Receipts.js
+++ src/compontes/Receipts.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React, { useState } from "react";
 
 import "./Receipts.css";
@@ -39,14 +40,23 @@ const Receipts = ({ person, order, paid }) => {
         <h3> {pill}</h3>
 =======
 import React from "react";
+=======
+import React, { useState } from "react";
+
+>>>>>>> c14778f (newUpdat onhpmework)
 import "./Receipts.css";
 
-const Receipts = ({ person, order }) => {
+const Receipts = ({ person, order, paid }) => {
+  const [pill, setPill] = useState(paid);
+  const chackPill = () => {
+    setPill((pill) => (pill ? "paid" : "notPid"));
+  };
   const { sauce, main, protein, rice, cost, drink, toppings } = order;
 
   return (
     <div className="continer">
       <div className="each-person">
+<<<<<<< HEAD
         <h1> {person}</h1>
         <h2> Main :{main}</h2>
         <h2> protein :{protein}</h2>
@@ -56,6 +66,32 @@ const Receipts = ({ person, order }) => {
         <h2>Toppings: {toppings} </h2>
         <h2> Cost :{cost}</h2>
 >>>>>>> 42c070d (done with javascript part)
+=======
+        <h1> {person.toUpperCase()}</h1>
+        <h3>
+          <div className="div-menu">Main</div> :{main}
+        </h3>
+        <h3>
+          <div className="div-menu">protein </div>:{protein}
+        </h3>
+        <h3>
+          <div className="div-menu">Rice </div> :{rice}
+        </h3>
+        <h3>
+          <div className="div-menu"> Sauce </div>:{sauce}
+        </h3>
+        <h3>
+          <div className="div-menu">Drink</div>:{drink}
+        </h3>
+        <h3>
+          <div className="div-menu">Toppings</div>: {toppings}
+        </h3>
+        <h3>
+          <div className="div-menu">Cost </div>:{cost}
+        </h3>
+        <button onClick={chackPill}> Paid ?</button>
+        <h3> {pill}</h3>
+>>>>>>> c14778f (newUpdat onhpmework)
       </div>
     </div>
   );
